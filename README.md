a6dc03dae59ec77620f07e14d191858626c1333c
Gotha <gotha@protonmail.com>
2021-03-28 15:36:23 +0300

    Changing architecture style to modules

--- README.md
+++
@@ -1,2 +0,0 @@
-# gl_part1
-gl_part1
--- VirtualMachines/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "VMTest" {
-  ami           = "ami-013f17f36f8b1fefb"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "VMTestUbuntu"
-  }
-}
