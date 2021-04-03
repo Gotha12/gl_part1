9c60978d2749aa4c19915e8fd8a3e90f98b68257
Gotha <gotha@protonmail.com>
2021-04-03 16:02:53 +0300

    Split modules

---
+++ Ec2/ec2.tf
@@ -0,0 +1,17 @@
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "my-ec2-cluster"
+  instance_count = 2
+
+  ami                    = "ami-013f17f36f8b1fefb"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
